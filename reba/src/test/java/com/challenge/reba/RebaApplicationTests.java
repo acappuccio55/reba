969bad4942175888f7c4acd069cdbfe2658da5d9
Agustin Cappuccio <108692144+acappuccio55@users.noreply.github.com>
2023-04-12 15:00:09 -0300

    Add files via upload

---
+++ reba/src/test/java/com/challenge/reba/RebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.reba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
